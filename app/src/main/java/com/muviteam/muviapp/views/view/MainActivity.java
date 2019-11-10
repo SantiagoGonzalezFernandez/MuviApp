824f10f6da742d56cfbd9460ff5af41bddaef588
Santiago <SantiagoGonzalezFernandezpro@gmail.com>
2019-11-10 15:19:14 -0300

    Agrego y modifico
    Cambio todos los ids y modifico el Navigation View

--- app/src/main/java/com/muviteam/muviapp/views/view/MainActivity.java
+++ app/src/main/java/com/muviteam/muviapp/views/view/MainActivity.java
@@ -1,16 +1,109 @@
 package com.muviteam.muviapp.views.view;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.SearchView;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.muviteam.muviapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private Toolbar myToolbar;
+    private ArrayAdapter<String> myArrayAdapterString;
+    private DrawerLayout myDrawerLayout;
+    private NavigationView myNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        encuentroVariablesPorId();
+        creoElAppBar();
+
+    }
+
+    private void encuentroVariablesPorId(){
+        myDrawerLayout = findViewById(R.id.MainActivity_DrawerLayout_Contenedor);
+        myNavigationView = findViewById(R.id.MainActivity_NavigationView);
+        myToolbar = findViewById(R.id.MainActivity_Include_Toolbar);
+    }
+
+
+    private void creoElAppBar(){
+        setSupportActionBar(myToolbar);
+
+        myArrayAdapterString = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.peliculas_array));
+
+        ActionBarDrawerToggle toggle =
+                new ActionBarDrawerToggle(this,
+                        myDrawerLayout,
+                        myToolbar,
+                        R.string.open_drawer,
+                        R.string.close_drawer);
+
+        myDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //le seteo el on click le digo this por que la main activity implementa el listener
+        myNavigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+        MenuItem mSearch = menu.findItem(R.id.ToolBarMenu_Item_action_search);
+
+        SearchView mSearchView = (SearchView) mSearch.getActionView();
+        mSearchView.setQueryHint("Search");
+
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                myArrayAdapterString.getFilter().filter(newText);
+                return true;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        //menuitem es el elemento seleccionado
+        Integer id = menuItem.getItemId();
+        //uso un switch para ponerle un comportamiento distinto a cada boton
+        switch (id) {
+            case R.id.MenuPrincipal_Item_Perfil:
+                Toast.makeText(this, "Entrado al perfil del usuario", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.MenuPrincipal_Item_Configuracion:
+                Toast.makeText(this, "Entrando a configuracion", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.MenuPrincipal_Item_CerrarSesion:
+                Toast.makeText(this, "Hasta Luego (Cerrar sesion)", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        myDrawerLayout.closeDrawers();
+        return true;
     }
 }
--- app/src/main/java/com/muviteam/muviapp/views/view/SplashActivity.java
+++ app/src/main/java/com/muviteam/muviapp/views/view/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
