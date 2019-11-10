6e622a251cd5674c42ed733aeb28400b9a4400e5
Juan C <juan.cisilino@gmail.com>
2019-11-10 14:50:53 -0300

    creacion de Fragments

---
+++ app/src/main/java/com/muviteam/muviapp/views/view/FragmentDetallePelicula.java
@@ -0,0 +1,27 @@
+package com.muviteam.muviapp.views.view;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.muviteam.muviapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentDetallePelicula extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detalle_pelicula, container, false);
+    }
+
+}
