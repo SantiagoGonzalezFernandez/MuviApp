e09746ba2952ee3b3d20d782773726661179fcb8
Santiago <SantiagoGonzalezFernandezpro@gmail.com>
2019-11-10 14:42:20 -0300

    Subo los cambio
    Modifique y agrege cosas en el res y el manifest

---
+++ app/src/main/java/com/muviteam/muviapp/views/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.muviteam.muviapp.views;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.muviteam.muviapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private VideoView videoViewVideoSplash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+       new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
+
+        videoViewVideoSplash = findViewById(R.id.SplashActivity_VideoView_VideoSplash);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.muvi;
+        Uri uri = Uri.parse(videoPath);
+        videoViewVideoSplash.setVideoURI(uri);
+        videoViewVideoSplash.requestFocus();
+        videoViewVideoSplash.start();
+    }
+}
