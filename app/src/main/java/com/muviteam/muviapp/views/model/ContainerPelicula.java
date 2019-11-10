4faa4640647335dde621b4f049a76d89c5b1d557
Juan C <juan.cisilino@gmail.com>
2019-11-10 15:39:49 -0300

    agrego codigo standard

--- app/src/main/java/com/muviteam/muviapp/views/model/ContainerPelicula.java
+++ app/src/main/java/com/muviteam/muviapp/views/model/ContainerPelicula.java
@@ -1,4 +1,15 @@
 package com.muviteam.muviapp.views.model;
 
+import java.util.List;
+
 public class ContainerPelicula {
+
+    private List<Pelicula> results;
+
+
+    public List<Pelicula> getResults() {
+        return results;
+    }
+
+
 }
