869f036112e74866f97ebc7da9092230174ed62d
Santiago <santiagomarinmaccione@gmail.com>
2019-11-16 17:27:00 -0300

    Celda y fragment generos (wip)

---
+++ app/src/main/java/com/muviteam/muviapp/view/generos.java
@@ -0,0 +1,33 @@
+package com.muviteam.muviapp.view;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.muviteam.muviapp.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class generos extends Fragment {
+
+
+    public generos() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_generos, container, false);
+    }
+
+}
