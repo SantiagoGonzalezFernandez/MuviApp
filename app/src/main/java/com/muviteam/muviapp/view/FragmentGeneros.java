9cfd164e02567729a087e8bc51af99a277f977af
Santiago <santiagomarinmaccione@gmail.com>
2019-11-21 17:55:45 -0300

    Fragment con las imagenes de los generos.
    Todavia no es clickable.

--- app/src/main/java/com/muviteam/muviapp/view/FragmentGeneros.java
+++ app/src/main/java/com/muviteam/muviapp/view/FragmentGeneros.java
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.muviteam.muviapp.R;
 
@@ -16,6 +18,9 @@ import com.muviteam.muviapp.R;
  * A simple {@link Fragment} subclass.
  */
 public class FragmentGeneros extends Fragment {
+
+    private ImageView accion,aventura,animacion,cienciaFiccion,comedia,crimen,documental,drama,familia,fantasia,guerra,historia,horror,misterio,musica,peliculasdetv,romance,western;
+    private View view;
 
 
     public FragmentGeneros() {
@@ -29,5 +34,49 @@ public class FragmentGeneros extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_generos, container, false);
     }
+
+    public void encontrarVariables(){
+
+        accion = view.findViewById(R.id.boton_genero1);
+
+        aventura = view.findViewById(R.id.boton_genero2);
+
+        animacion = view.findViewById(R.id.boton_genero3);
+
+        cienciaFiccion = view.findViewById(R.id.boton_genero4);
+
+        comedia = view.findViewById(R.id.boton_genero5);
+
+        crimen = view.findViewById(R.id.boton_genero6);
+
+        documental = view.findViewById(R.id.boton_genero7);
+
+        drama = view.findViewById(R.id.boton_genero8);
+
+        familia = view.findViewById(R.id.boton_genero9);
+
+        fantasia = view.findViewById(R.id.boton_genero10);
+
+        guerra = view.findViewById(R.id.boton_genero11);
+
+        historia = view.findViewById(R.id.boton_genero12);
+
+        horror = view.findViewById(R.id.boton_genero13);
+
+        misterio = view.findViewById(R.id.boton_genero14);
+
+        musica = view.findViewById(R.id.boton_genero15);
+
+        peliculasdetv = view.findViewById(R.id.boton_genero16);
+
+        romance = view.findViewById(R.id.boton_genero17);
+
+        western = view.findViewById(R.id.boton_genero18);
+
+
+    }
+
+
+
 
 }
